084743e191770c669a6aebb23358466b685f0514
Airplane <213837125+TheAirplaneMan@users.noreply.github.com>
2025-10-16 23:00:25 -0800

    Add Lanterns

---
+++ mods/tstdrm_decor/init.lua
@@ -0,0 +1,32 @@
+minetest.register_node("tstdrm_decor:lantern", { -- Borrowed from WintersKnight94
+	description = "Lantern",
+		tiles = {
+		"tstdrm_chrome.png",
+		"tstdrm_chrome.png",
+		"tstdrm_lantern_side.png",
+	},
+	drawtype = "nodebox",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.2500, -0.5000, -0.2500, 0.2500, -0.4375, 0.2500},
+			{-0.2500, 0.000, -0.2500, 0.2500, 0.06250, 0.2500},
+			{-0.1875, -0.5000, -0.1875, 0.1875, 0.1250, 0.1875}
+		}
+	},
+	paramtype = "light",
+	sunlight_propagates = true,
+	light_source = 15,
+	groups = { oddly_breakable_by_hand = 3},
+--	sounds
+})
+
+minetest.register_craft({
+	output = "tstdrm_decor:lantern 4",
+	recipe = {
+		{"", "tstdrm_core:chrome", ""},
+		{"tstdrm_core:chrome", "tstdrm_core:wood", "tstdrm_core:chrome"},
+		{"", "tstdrm_core:chrome", ""},
+
+	}
+})
